bbdc4b99d7d37c478d5887af3b2035e4acf921c1
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-04-04 18:20:24 +0300

    created one model song and admin panel

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Song(models.Model):
+    """Song model"""
+    title = models.CharField(max_length=100)
+    albums = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='song_photos/', blank=True)
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
