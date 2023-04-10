6f1557d93bad068f8aa0ff923dd81d956cd58d45
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-04-10 23:40:32 +0300

    added .env file, installed dotenv and environ libraries, hid secret key

--- my_music/settings.py
+++ my_music/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--thkts9b_-k6w0dp(2#@!4&twxq(3+=(9(d&8*_k@-jrpv43-w'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
